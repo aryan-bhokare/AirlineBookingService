206c5bb3c544a893fcbddaf26d66bfec5dcc114d
Aryan <aryan1bhokare@gmail.com>
2023-05-05 10:37:17 +0530

    first commit basic skeleton done

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,16 @@
+const { StatusCodes } = require('http-status-codes');
+
+class ServiceError extends Error {
+    constructor(
+        message = 'something went wrong',
+        explanation = 'service layer error',
+        statusCodes = StatusCodes.INTERNAL_SERVER_ERROR
+    ){
+        this.name = 'ServiceError';
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCodes = statusCodes  
+    }
+}
+
+module.exports = ServiceError;
